c33ff85de0a977d52f2807b14929e3bdb1844051
aliserhan315 <aliserhan315@gmail.com>
2025-07-29 01:37:07 +0300

    made the book and the order models and edited the databse adding 3 tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Notifications\DatabaseNotification;
+
+
 
 class User extends Authenticatable
 {
@@ -45,5 +48,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+        public function notifications()
+    {
+        return $this->morphMany(DatabaseNotification::class, 'notifiable');
     }
 }
--- database/migrations/2025_07_28_195902_create_books_table.php
+++ database/migrations/2025_07_28_195902_create_books_table.php
@@ -11,20 +11,44 @@ return new class extends Migration
      */
     public function up(): void
     {
-    Schema::create('books', function (Blueprint $table) {
-    $table->id();
-    $table->string('title');
-    $table->string('author');
-    $table->string('category')->nullable();
-    $table->string('publisher')->nullable();
-    $table->year('published_year')->nullable();
-    $table->text('description')->nullable();
-    $table->decimal('price', 8, 2);
-    $table->integer('stock')->default(0);
-    $table->string('image')->nullable();
-    $table->boolean('is_available')->default(true);
-    $table->timestamps();
-});
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');     
+            $table->string('publisher')->nullable();
+            $table->year('published_year')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->integer('stock')->default(0);
+            $table->string('image')->nullable();
+            $table->integer('sold')->default(0); 
+            $table->boolean('is_available')->default(true);
+            $table->timestamps();
+            $table->integer('rating');
+        });
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+        Schema::create('book_category', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+        });
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('book_id');
+            $table->text('comment');
+            $table->integer('rating');
+            $table->timestamps();
+        });
+
+
+
     }
 
     /**
@@ -32,6 +56,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('books');
+    Schema::dropIfExists('reviews');
+    Schema::dropIfExists('book_category');
+    Schema::dropIfExists('categories');
+    Schema::dropIfExists('books');
     }
 };
