3ee3a8dbe1b10f81e9999be293b4ea3d7b187bc3
MarkSemaan <mark.h.semaan@gmail.com>
2025-07-30 22:42:20 +0300

    created user service  with empty functions

---
+++ backend/app/Services/UserService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+
+class UserService
+{
+    public function registerUser() {}
+
+    public function loginUser() {}
+
+    public function deactivateUser() {}
+
+    public function getUserByID() {}
+}
